77d07ae9c03739e8703d93a532aa3ab3c44cb3c4
Rutger Willems <rut.willems@gmail.com>
2019-10-09 10:59:43 +0200

    blind quick commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,16 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        devsync-website
+        DevSync coming soon...
       </h1>
-      <h2 class="subtitle">
-        website for the devsync plugin
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
+  components: {}
 }
 </script>
 
@@ -54,24 +30,10 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
+  font-weight: 400;
+  font-size: 24px;
   letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  font-family: 'Courier New', Courier, monospace;
 }
 </style>
