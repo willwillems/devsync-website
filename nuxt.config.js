d7db88738dca538b25a36f6e071d798311154d2d
Rutger Willems <rut.willems@gmail.com>
2019-12-01 14:50:51 +0000

    add og/fb/tw meta info+img

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,25 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Devsync',
+    title: 'Devsync | Visually edit your CSS in-browser.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'title', name: 'title', content: 'Devsync | Visually edit your CSS in-browser.' },
       { hid: 'description', name: 'description', content: 'A modern visual design tool that live-sync\'s CSS edits with your IDE. Speed up your workflow now!' },
-      { name: 'theme-color', content: '#7CBBFF' }
+      { name: 'theme-color', content: '#7CBBFF' },
+      // -- Open Graph / Facebook --
+      { property: 'og:type',  content: 'website' },
+      { property: 'og:url',  content: 'https://devsync.co/' },
+      { property: 'og:title',  content: 'Devsync | Visually edit your CSS in-browser.' },
+      { property: 'og:description',  content: 'Visual design tool that live-sync\'s CSS edits with your editor. Design quicker. Devsync uses the browser\'s debugger to find your CSS and edit it in real time.' },
+      { property: 'og:image',  content: 'https://devsync.co/img/social-card.png' },
+      // -- Twiter --
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:url', content: 'https://devsync.co/' },
+      { property: 'twitter:title', content: 'Devsync | Visually edit your CSS in-browser.' },
+      { property: 'twitter:description', content: 'Visual design tool that live-sync\'s CSS edits with your editor. Design quicker. Devsync uses the browser\'s debugger to find your CSS and edit it in real time.' },
+      { property: 'twitter:image', content: 'https://devsync.co/img/social-card.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
