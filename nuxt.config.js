64030196c56af0fca961ca32de7c8511f7bcae2e
Rutger Willems <rut.willems@gmail.com>
2020-02-13 10:48:27 +0200

    add drift messaging

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
   */
   head: {
     title: 'Devsync | Visually edit your CSS in-browser.',
+    script: [{
+      src: '/js/drift.js'
+    }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
